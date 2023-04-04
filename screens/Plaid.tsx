e1b11479f6fcfe5e97e5b2b86d197b44669c1124
Eddie Lopez <ejlopez00@gmail.com>
2023-04-04 15:06:57 -0400

    refactor

--- screens/Plaid.tsx
+++ screens/Plaid.tsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 import Constants from "expo-constants";
-import React, { useState, useEffect } from 'react'
+import React, { useState, useEffect } from "react";
 import { View, Text, Dimensions } from "react-native";
 import {
     ArrowDownLeftIcon,
@@ -9,46 +9,39 @@ import {
     ChartPieIcon,
 } from "react-native-heroicons/solid";
 
-// @ts-expect-error
-import PlaidLink from '@burstware/expo-plaid-link'
+import { LinkExit, LinkSuccess, PlaidLink } from "react-native-plaid-link-sdk";
 
-
+// @ts-ignore
 import MainContainer from "../components/Container/MainContainer";
 
 const Plaid = () => {
     //const [isLoading, setLoading] = React.useState(true);
-    const [linkToken, setLinkToken] = React.useState<string | 'null'>("linkToken");
+    const [linkToken, setLinkToken] = React.useState<string | "null">("linkToken");
 
     useEffect(() => {
         getLinkToken();
-    }, [])
+    }, []);
 
     const getLinkToken = () => {
         axios
-            .get(`${Constants.manifest!.extra!.backendUri}/api/v0/plaid/link-token/create`, {
-            })
+            .get(`${Constants.manifest!.extra!.backendUri}/api/v0/plaid/link-token/create`, {})
             .then((response) => {
                 //console.log(response.data.data)
-                setLinkToken(response.data.data.link_token)
+                setLinkToken(response.data.data.link_token);
             })
             .catch((err) => console.log(err.response.data));
-    }
-
+    };
 
     console.log(linkToken);
 
-
     return (
-        
         <MainContainer>
-            
             <PlaidLink
-            linkToken={linkToken}
-            onEvent={console.log("event")}
-            onExit={console.log("exit")}
-            onSuccess={console.log("success.publicToken")}
-            onError={console.log("error")}
-             />
+                tokenConfig={{ token: linkToken, noLoadingState: false }}
+                onSuccess={(success: LinkSuccess) => console.log(success)}
+                onExit={(exit: LinkExit) => console.log(exit)}>
+                <Text>Add Account</Text>
+            </PlaidLink>
         </MainContainer>
     );
 };
